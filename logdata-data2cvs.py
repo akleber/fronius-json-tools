3250bfda12c190e7bb54c14f1037ad4d0056dc51
Andreas Kleber <aln@esi-group.com>
2016-09-15 19:40:26 +0200

    first nice version

--- logdata-data2cvs.py
+++ logdata-data2cvs.py
@@ -10,16 +10,20 @@ with open('examples/logdata-data20160913235000.json') as data_file:
 #pprint(data)
 #print(data['Body']['inverter/1']['Data']['Current_DC_String_1']['Values'])
 
-values = data['Body']['inverter/1']['Data']['Current_DC_String_1']['Values']
-orderedValues = collections.OrderedDict(sorted(values.items()))
+c_dc_1_values = data['Body']['inverter/1']['Data']['Current_DC_String_1']['Values']
 
+# Dictionaraies, returned by the json reader, have no order.
+# So, convert it to a list of key/value tupels (.items()) and
+# sort it with sorted(), using a lambda function to convert the key/first tupel item to an integer
+# so that the sorting is based on numbers not text
+c_dc_1_values_ordered = sorted(c_dc_1_values.items(), key=lambda x: int(x[0]) )
 
-with open('examples/logdata-data20160913235000.json.csv', 'wb') as csvfile:
-    #data_writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #data_writer.writerow(data['Body']['inverter/1']['Data']['Current_DC_String_1']['Values'])
+# no header text for the time column so that excel treats it as x axis values
+fieldnames = ['', 'Current_DC_String_1']
 
-    fieldnames = orderedValues.keys()
+with open('examples/logdata-data20160913235000.json.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file, dialect='excel-tab')
+    writer.writerow(fieldnames)
 
-    data_writer = csv.DictWriter(csvfile, dialect='excel', fieldnames=fieldnames)
-    data_writer.writeheader()
-    data_writer.writerow(orderedValues)
+    for key, value in c_dc_1_values_ordered:
+       writer.writerow([key, value])
