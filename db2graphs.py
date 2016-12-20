3aa4a026faa92f6d729cd30820bf07d66bc42194
Andreas Kleber <andreas@drosselweg7a.de>
2016-12-20 12:57:33 +0100

    improves query

--- db2graphs.py
+++ db2graphs.py
@@ -7,7 +7,7 @@ con = lite.connect('examples/fronius.db')
 with con:
     cur = con.cursor()    
     
-    cur.execute("SELECT timestamp, powerflow_P_PV from fronius")
+    cur.execute("SELECT 'timestamp', 'powerflow_P_PV' FROM fronius WHERE 'timestamp' BETWEEN '2016-09-07' AND '2016-09-08'")
     rows = cur.fetchall() 
 
 x = [ row[0] for row in rows ]
