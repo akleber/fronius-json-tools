d5caa6d2ed91e191bd5a340420f68e365c35168c
Andreas Kleber <andreas@drosselweg7a.de>
2017-01-02 22:24:31 +0100

    Tab2space, improved file naming

--- archivedata2json.py
+++ archivedata2json.py
@@ -19,52 +19,55 @@ from datetime import timedelta, date, datetime
 hostname = "fronius"
 
 def daterange(start_date, end_date):
-	for n in range(int ((end_date - start_date).days + 1)):
-		yield start_date + timedelta(n)
+    for n in range(int ((end_date - start_date).days + 1)):
+        yield start_date + timedelta(n)
 
 def main(argv):
-	subdir = argv[3]
+    subdir = argv[3]
 
-	#start_date = date(2016, 11, 1)
-	#end_date = date(2016, 11, 1)
+    if not os.path.exists(subdir):
+        os.makedirs(subdir)
 
-	start_date = datetime.strptime(argv[1], "%d.%m.%Y")
-	end_date = datetime.strptime(argv[2], "%d.%m.%Y")
+    #start_date = date(2016, 11, 1)
+    #end_date = date(2016, 11, 1)
 
-	print("Start data: " + start_date.strftime("%d.%m.%Y"))
-	print("End date " + end_date.strftime("%d.%m.%Y"))
-	print("Subdir: " + subdir)
+    start_date = datetime.strptime(argv[1], "%d.%m.%Y")
+    end_date = datetime.strptime(argv[2], "%d.%m.%Y")
 
-	for single_date in daterange(start_date, end_date):
-		datestring = single_date.strftime("%d.%m.%Y")
-		print("Daily Sum for: " + datestring)
+    print("Start data: " + start_date.strftime("%d.%m.%Y"))
+    print("End date " + end_date.strftime("%d.%m.%Y"))
+    print("Subdir: " + subdir)
 
-		url = "http://" + hostname + "/solar_api/v1/GetArchiveData.cgi?Scope=System&StartDate=" + \
-			datestring + "&EndDate=" + datestring + "&SeriesType=DailySum&Channel=EnergyReal_WAC_Sum_Produced&Channel=InverterErrors"
+    for single_date in daterange(start_date, end_date):
+        datestring = single_date.strftime("%d.%m.%Y")
+        datestring_reversed = single_date.strftime("%Y-%m-%d")
+        print("Daily Sum for: " + datestring)
 
-		r = requests.get(url, timeout=120)
-		r.raise_for_status()
-		jsondata = r.json()
+        url = "http://" + hostname + "/solar_api/v1/GetArchiveData.cgi?Scope=System&StartDate=" + \
+            datestring + "&EndDate=" + datestring + "&SeriesType=DailySum&Channel=EnergyReal_WAC_Sum_Produced&Channel=InverterErrors"
 
-		with open(os.path.join(subdir, "archivedata_dailysum_" + datestring + ".json"), 'w') as f:
-			json.dump(jsondata, f, indent=4)
+        r = requests.get(url, timeout=120)
+        r.raise_for_status()
+        jsondata = r.json()
+
+        with open(os.path.join(subdir, "archivedata_dailysum_" + datestring_reversed + ".json"), 'w') as f:
+            json.dump(jsondata, f, indent=4)
 
 
-		print("Day : " + datestring)
-		url = "http://" + hostname + "/solar_api/v1/GetArchiveData.cgi?Scope=System&StartDate=" + \
-			datestring + "&EndDate=" + datestring + "&Channel=EnergyReal_WAC_Plus_Absolute&Channel=EnergyReal_WAC_Minus_Absolute"
+        print("Day : " + datestring)
+        url = "http://" + hostname + "/solar_api/v1/GetArchiveData.cgi?Scope=System&StartDate=" + \
+            datestring + "&EndDate=" + datestring + "&Channel=EnergyReal_WAC_Plus_Absolute&Channel=EnergyReal_WAC_Minus_Absolute"
 
-		r = requests.get(url, timeout=120)
-		r.raise_for_status()
-		jsondata = r.json()
+        r = requests.get(url, timeout=120)
+        r.raise_for_status()
+        jsondata = r.json()
 
-		with open(os.path.join(subdir, "archivedata_day_" + datestring + ".json"), 'w') as f:
-			json.dump(jsondata, f, indent=4)
+        with open(os.path.join(subdir, "archivedata_day_" + datestring_reversed + ".json"), 'w') as f:
+            json.dump(jsondata, f, indent=4)
 
-		print("Finished day: " + datestring)
-		time.sleep(3)
+        print("Finished day: " + datestring)
+        time.sleep(3)
 
 if __name__ == "__main__":
-	main(sys.argv)
-
+    main(sys.argv)
 
